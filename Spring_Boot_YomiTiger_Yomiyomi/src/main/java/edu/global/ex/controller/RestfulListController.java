5a6834b3dc4fea53899319715dd5e60d167c2089
John <1996tjrgns@naver.com>
2023-01-05 12:18:44 +0900

    JOHN FIRST COMMIT VO, MAPPER, SERVICE, CONTROLLER, PAGE, SECURITY

---
+++ Spring_Boot_YomiTiger_Yomiyomi/src/main/java/edu/global/ex/controller/RestfulListController.java
@@ -0,0 +1,18 @@
+package edu.global.ex.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class RestfulListController {
+	
+	@GetMapping("/test")
+	public String home() {
+		log.info("/test");
+		
+		return "test";
+	}
+
+}
---
+++ Spring_Boot_YomiTiger_Yomiyomi/src/main/java/edu/global/ex/mapper/LoginMapper.java
@@ -0,0 +1,4 @@
+package edu.global.ex.mapper;
+
+public interface LoginMapper {
+}
