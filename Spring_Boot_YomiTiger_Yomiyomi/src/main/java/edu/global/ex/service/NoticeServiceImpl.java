22ee442d3bbf735c6af200d5d9e96736a45bf900
John <1996tjrgns@naver.com>
2023-01-17 13:09:50 +0900

    공지 페이지 기능

--- Spring_Boot_YomiTiger_Yomiyomi/src/main/java/edu/global/ex/service/NoticeServiceImpl.java
+++ Spring_Boot_YomiTiger_Yomiyomi/src/main/java/edu/global/ex/service/NoticeServiceImpl.java
@@ -1,6 +1,7 @@
 package edu.global.ex.service;
 
 import edu.global.ex.mapper.NoticeMapper;
+import edu.global.ex.page.Criteria;
 import edu.global.ex.vo.BoardVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,17 @@ public class NoticeServiceImpl implements NoticeService {
     public BoardVO read(int id){
         log.info("read()..");
         return noticeMapper.read(id);
+    }
+
+    @Override
+    public int getTotal() {
+        log.info("getTotal() ..");
+        return noticeMapper.getTotalCount();
+    }
+
+    @Override
+    public List<BoardVO> getList(Criteria criteria) {
+        log.info("getList(Criteria criteria) ..");
+        return noticeMapper.getListWithPaging(criteria);
     }
 }
